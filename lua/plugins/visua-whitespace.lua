320ffec4b3fbf52f3274ebe1cdde15ee5f914ff6
js-bruno <brunocebr@hotmail.com>
2024-11-17 18:39:21 -0300

    feature: removing harpoon, bufferline and lualine. Adding windline with tabs and arrow for marking

---
+++ lua/plugins/visua-whitespace.lua
@@ -0,0 +1,12 @@
+return {
+  "mcauley-penney/visual-whitespace.nvim",
+  enabled = true,
+  config = true,
+  opts = {
+    highlight = { link = "Visual" },
+    space_char = "·",
+    tab_char = "→",
+    nl_char = "↲",
+    cr_char = "←",
+  },
+}
