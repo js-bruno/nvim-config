1de669dd06b55bb8df71557f84aa86744fc48297
js-bruno <brunocebr@hotmail.com>
2025-03-09 09:11:16 -0300

    feature: adding listchars toggle, adding nvim-pqf and changing nvim-surround to mini-surround

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -15,14 +15,26 @@ return {
       },
     },
     config = function()
-      require("lspconfig").lua_ls.setup{}
-      require("lspconfig").gopls.setup{}
-      require'lspconfig'.pylsp.setup{
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+      local init_options = {
+        configurationSection = { "html", "css", "javascript" },
+        embeddedLanguages = {
+          css = true,
+          javascript = true
+        },
+        provideFormatter = true
+      }
+      require("lspconfig").html.setup { capabilities = capabilities, init_options = init_options }
+      require("lspconfig").lua_ls.setup {}
+      require("lspconfig").gopls.setup {}
+      require 'lspconfig'.pylsp.setup {
         settings = {
           pylsp = {
             plugins = {
               pycodestyle = {
-                ignore = {'W391'},
+                ignore = { 'W391' },
                 maxLineLength = 100
               }
             }
@@ -31,20 +43,26 @@ return {
       }
 
       vim.api.nvim_create_autocmd('LspAttach', {
-        callback = function (args)
+        callback = function(args)
           local client = vim.lsp.get_client_by_id(args.data.client_id)
           if not client then return end
 
           -- lsp keymaps
           vim.keymap.set("n", "K", vim.lsp.buf.signature_help, {})
           -- vim.keymap.set("n",, vim.lsp.buf.hover, {})
-          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {desc="Go To Declaration"})
-          vim.keymap.set("n", "gd", vim.lsp.buf.definition, {desc="Go to Definition"})
-          vim.keymap.set("n", "gI", vim.lsp.buf.implementation, {desc="Go to Implementation"})
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go To Declaration" })
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
+          vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { desc = "Go to Implementation" })
           vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
           vim.keymap.set("n", "ge", vim.lsp.buf.references, {})
           vim.keymap.set("n", "gr", vim.lsp.buf.rename, {})
 
+          vim.keymap.set("n", "<space>bf", vim.lsp.buf.format, {})
+
+
+          vim.keymap.set("n", "<leader>o", vim.diagnostic.setloclist)
+          vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+          vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
           -- Format file when saves the documetn
           -- if client.supports_method('textDocument/formatting') then
           --   vim.api.nvim_create_autocmd('BufWritePre', {
@@ -56,6 +74,6 @@ return {
           -- end
         end
       })
-   end,
+    end,
   },
 }
