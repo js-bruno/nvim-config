782b8c51bf84723831f44d555838ccc70c1c5e34
js-bruno <brunocebrsilva@gmail.com>
2025-07-13 21:09:10 -0300

    fix: adjusting download vim command

--- install_nvim.sh
+++ install_nvim.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+#set -o errexit
+#set -o setnounset
+#set -o piperfail
+#set -o xtrace
+
+# RUN THIS FIRST
+# export PATH="$PATH:HOME/bin"
+# export PATH=$PATH:~/bin
+# echo -e 'export PATH="$PATH:HOME/bin"' >> "$HOME/.bashrc"
+
+mv ./ "$HOME/.config/nvim"
+source "$HOME/.bashrc"
+
 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
-chmod u+ nvim-linux-x86_64.appimage
-mkdir -p /opt/nvim
+chmod +X nvim-linux-x86_64.appimage
 
-touch ~/bin/nvim | echo -e '/opt/nvim/nvim-linux-x86_64.appimage' >> ~/bin/nvim
+mkdir -p "$HOME/bin/"
+
+ln -s ./nvim-linux-x86_64.appimage "$HOME/bin/nvim"
+
+# touch ~/bin/nvim | echo -e '/opt/nvim/nvim-linux-x86_64.appimage' >> ~/bin/nvim
